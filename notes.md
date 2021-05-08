cde4183b0e619e3a0d63f8975d39b3f0a9c586d0
Élise Philippe <elise@eiss.fr>
2021-05-08 23:16:03 +0200

    working db insert

---
+++ notes.md
@@ -0,0 +1,10 @@
+## custom type and data validation
+
+https://github.com/altostra/type-validations
+https://www.altostra.com/blog/data-validation
+
+## typescript type guarding
+
+https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
+https://fettblog.eu/typescript-hasownproperty/
+https://devblogs.microsoft.com/typescript/announcing-typescript-4-0-beta/#unknown-on-catch
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,53 @@ function hello() {
 }
 
 hello();
+
+interface user {
+  name: string;
+  gender: string;
+}
+
+import { r, RDatum, RTable, RethinkDBError, RethinkDBErrorType } from "rethinkdb-ts";
+
+function isRethinkDBError(x: any): x is RethinkDBError {
+  return typeof x.type == "number";
+}
+
+r.connect({ server: { host: "127.0.0.1", port: 49154 } })
+  .catch(() => {
+    console.log("couldn't connect but that's expected");
+  })
+  .then(async (conn) => {
+    console.log("it worked");
+
+    if (conn) {
+      // console.log(conn);
+      try {
+        await r.db("test").tableCreate("users").run(conn);
+      } catch (err) {
+        if (isRethinkDBError(err)) {
+          if (err.type == RethinkDBErrorType.OP_FAILED) console.log("already exists");
+        }
+      }
+      let table: RTable<user> = r.db("test").table("users");
+
+      let tmp: user = {
+        name: "Douce",
+      };
+
+      await table.insert({ name: "Élise", gender: "no" }).run(conn);
+      await table.insert({ name: "Potate", gender: "probably" }).run(conn);
+      await table.insert({ name: "Douce" }).run(conn);
+
+      let users = await table
+        // .table<RDatum<{ name: string; gender: string }>>("prout")
+        .filter((doc) => doc("name").eq("Élise"))
+        .run(conn);
+
+      console.log(users);
+
+      conn.close().then(() => {
+        console.log("closed");
+      });
+    }
+  });
